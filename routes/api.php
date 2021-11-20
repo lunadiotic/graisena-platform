bfcc47eb0be4ffbe0a9e0171bf8b699fe60ec691
hakim <ahmadhakim404@gmail.com>
2021-11-19 18:29:26 -0800

    change background volunteer reg page

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('location/regencies', 'Api\LocationController@regency');
+Route::post('location/regencies', 'Api\LocationController@regency')->name('api.location.regencies');
